8ad6084d4c60b88110a72d58db78fd7555b2b21d
Yannic Kilcher <yannic.kilcher@gmail.com>
2016-10-03 00:55:34 +0200

    gpu

--- util/install_torch_and_fb_rocks.sh
+++ util/install_torch_and_fb_rocks.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-curl https://raw.githubusercontent.com/yk/ansible/master/roles/fb-torch-rocks-cuda-75/tasks/main.yml > fbtasks.yml
+curl https://raw.githubusercontent.com/yk/ansible/master/roles/fb-torch-rocks-cuda/tasks/main.yml > fbtasks.yml
 curl https://raw.githubusercontent.com/yk/ansible/master/util/install_torch_and_fb_rocks.yml > fbpb.yml
 
 ansible-playbook fbpb.yml -c local -i localhost,
